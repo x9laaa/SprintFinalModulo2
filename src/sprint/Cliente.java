70b6760f5508a1d908f0d9bd0a7b9ed108e3be4e
x9laaa <acarreno95@outlook.com>
2024-08-01 22:06:49 -0400

    creacion de interface Asesoría, clases usuario y cliente

---
+++ src/sprint/Cliente.java
@@ -0,0 +1,88 @@
+package sprint;
+
+import java.time.LocalDate;
+
+public class Cliente extends Usuario {
+    private String telefono;
+    private String afp;
+    private int sistemaSalud; // 1 (Fonasa) o 2 (Isapre)
+    private String direccion;
+    private String comuna;
+    private int edad;
+
+    public Cliente() {
+    }
+
+    public Cliente(int rut, String nombre, String apellido, LocalDate fechaNacimiento, String telefono, String afp, int sistemaSalud, String direccion, String comuna, int edad) {
+        super(rut, nombre, apellido, fechaNacimiento);
+        this.telefono = telefono;
+        this.afp = afp;
+        this.sistemaSalud = sistemaSalud;
+        this.direccion = direccion;
+        this.comuna = comuna;
+        this.edad = edad;
+    }
+
+    public String obtenerNombre() {
+        return getNombre() + " " + getApellido();
+    }
+
+    public String obtenerSistemaSalud() {
+        return sistemaSalud == 1 ? "Fonasa" : "Isapre";
+    }
+
+    @Override
+    public void analizarUsuario() {
+        super.analizarUsuario();
+        System.out.println("Dirección: " + direccion + ", Comuna: " + comuna);
+    }
+
+    // Getters y Setters
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getAfp() {
+        return afp;
+    }
+
+    public void setAfp(String afp) {
+        this.afp = afp;
+    }
+
+    public int getSistemaSalud() {
+        return sistemaSalud;
+    }
+
+    public void setSistemaSalud(int sistemaSalud) {
+        this.sistemaSalud = sistemaSalud;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
---
+++ src/sprint/Usuario.java
@@ -0,0 +1,67 @@
+package sprint;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Usuario implements Asesoria {
+
+    private int rut;
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+
+    public Usuario() {
+    }
+
+    public Usuario(int rut, String nombre, String apellido, LocalDate fechaNacimiento) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String mostrarEdad() {
+        int edad = Period.between(fechaNacimiento, LocalDate.now()).getYears();
+        return "El usuario tiene " + edad + " años";
+    }
+
+    @Override
+    public void analizarUsuario() {
+        System.out.println("Nombre: " + nombre + " " + apellido + ", RUT: " + rut);
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    
+
+}
