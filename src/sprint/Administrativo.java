ac86372fd5d2dfc1075ef2d789ddaf5ce7f70b44
Max Jeldres <mjeldres@prodalam.cl>
2024-08-06 22:54:18 -0400

    administrativo class add

--- src/sprint/Administrativo.java
+++ src/sprint/Administrativo.java
@@ -1,5 +1,60 @@
 package sprint;
 
-public class Administrativo {
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class Administrativo extends Usuario {
+    private String area;
+    private Optional<String> experiencia;
+
+    public Administrativo(int rut, String nombre, String apellido, LocalDate fechaNacimiento) {
+        super(rut, nombre, apellido, fechaNacimiento);
+        if (validLongArea(area.length())) {
+            this.area = area;
+        } else {
+            throw new IllegalArgumentException("Area entre 5 y 20 caract.");
+        }
+
+        if (experiencia.isPresent() && validLongExp(experiencia.get().length())) {
+            this.experiencia = experiencia;
+        } else {
+            throw new IllegalArgumentException("max 100 caract en exp.");
+        }
+    }
+
+    public Administrativo() {
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        if (validLongArea(area.length())) {
+            this.area = area;
+        } else {
+            throw new IllegalArgumentException("Area entre 5 y 20 caract.");
+        }
+    }
+
+    public Optional<String> getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(Optional<String> experiencia) {
+        if (experiencia.isPresent() && validLongExp(experiencia.get().length())) {
+            this.experiencia = experiencia;
+        } else {
+            throw new IllegalArgumentException("max 100 caract en exp.");
+        }
+    }
+
+    private boolean validLongArea(int lng) {
+        return lng >= 5 && lng <= 20;
+    }
+
+    private boolean validLongExp(int lng) {
+        return lng <= 100;
+    }
 
 }
